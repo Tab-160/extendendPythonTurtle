39f0a2fea1b5fee30efb961cf82b3159c165d7e3
Tab-160 <43759848+Tab-160@users.noreply.github.com>
2019-05-31 08:31:28 -0700

    Update moreTurtle.py
    Added toggleColor and rectangle functions.
    Fixed bug where filledSquare always made a square of length 94.

--- moreTurtle.py
+++ moreTurtle.py
@@ -1,5 +1,9 @@
 import turtle
 
+color1 = "#0000ff"
+color2 = "#00ff00"
+colorToggle = 0
+
 def setUp():
 	turtle.penup()
 	turtle.goto(-750,375)
@@ -7,11 +11,39 @@ def setUp():
 	turtle.shape("turtle")
 
 def square(length):
-	for i in range(4):
-	turtle.forward(length)
-		turtle.right(90)
+	rectangle(length, length)
 
 def filledSquare(length):
 	turtle.begin_fill;
-	square(94)
+	square(length)
 	turtle.end_fill;
+	
+def rectangle(length, width):
+	for i in range(2):
+		turtle.forward(length)
+		turtle.right(90)
+		turtle.forward(width)
+		turtle.right(90)
+
+def filledRectangle(length, width):
+	turtle.begin_fill;
+	rectangle(length, width)
+	turtle.end_fill;
+	
+def toggleSetUp(setColor1, setColor2):
+	global color1
+	global color2
+	color1 = setColor1
+	color2 = setColor2
+	
+def toggleColor():
+	global colorToggle
+	global color1
+	global color2
+	
+	if colorToggle == 0:
+		turtle.color(color1)
+		colorToggle = 1
+	else:
+		turtle.color(color2)
+		colorToggle = 0
